80525622218ebd04c36da044b196080f9c2f45ca
nainamahey <maheynaina@gmail.com>
2021-04-05 14:50:06 +0530

    fish

--- app/src/main/java/com/example/rentcar/login.java
+++ app/src/main/java/com/example/rentcar/login.java
@@ -28,7 +28,7 @@ public class login extends AppCompatActivity {
     boolean valid = true;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-
+//FUCKKKKKSJKJDKSJKLDLS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
